f08f0d62b15db465cbe85404cd65f957edc10db4
BuildersRejected <buildersrejected@gmail.com>
2021-12-22 15:27:45 -0600

    remove token

--- index.js
+++ index.js
@@ -28,4 +28,4 @@ client.once('ready', () => {
 
 // Login to Discord with your client's token
 //client.login(token);
-client.login('OTIzMzEwNTU1MTUyOTIwNTk3.YcOJ9g.677rvjTtBiuLrUV6XGF8RxmWB18');
+client.login('');
